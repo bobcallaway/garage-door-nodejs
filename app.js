1a8765729b157a1185ae441ea04494ec54e0071c
Bob Callaway <bob.callaway@gmail.com>
2013-10-09 10:37:31 -0400

    add push, clean up

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-var AWS = require('aws-sdk'), util = require('util'), async = require('async');
-//var gpio = require('pi-gpio');
+var AWS = require('aws-sdk'), util = require('util'), async = require('async'), pushover = require('node-pushover'), gpio = require('pi-gpio'); 
 
-// configure AWS
 AWS.config.loadFromPath('./config/aws_config.json');
-//AWS.config.update({ 'region': 'us-east-1', 'accessKeyId': 'AKIAIPADUMAIHXBKKNKQ', 'secretAccessKey': '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' });
 
 var sqs = new AWS.SQS().client;
+var push = new pushover({ token: pushover_token, user: pushover_user });
 
-//function delayPinWrite(pin, value, callback) {
-//    setTimeout(function() {
-//        gpio.write(pin, value, callback);
-//    }, config.RELAY_TIMEOUT);
-//}
+var config = {
+    PIN: 7,
+    RELAY_ON: 0,
+    RELAY_OFF: 1,
+    RELAY_TIMEOUT: 5
+};
 
-process.on( 'SIGINT', function() {
-    console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
-    // some other closing procedures go here
-    process.exit( );
-})
+function delayPinWrite(pin, value, callback) {
+    setTimeout(function() {
+        gpio.write(pin, value, callback);
+    }, config.RELAY_TIMEOUT);
+}
 
 function readMessage() {
     sqs.receiveMessage({
@@ -30,30 +29,31 @@ function readMessage() {
             if ((data.Messages) && (typeof data.Messages[0] !== 'undefined' && typeof data.Messages[0].Body !== 'undefined')) {
                 //sqs msg body
                 sqs_message_body = JSON.parse(data.Messages[0].Body);
-                console.log(sqs_message_body);
-                /* async.series([
-                        function(callback) {
-                        // Open pin for output
-                        gpio.open(config.LEFT_GARAGE_PIN, "output", callback);
-                        },
-                        function(callback) {
-                        // Turn the relay on
-                        gpio.write(config.LEFT_GARAGE_PIN, config.RELAY_ON, callback);
-                        },
-                        function(callback) {
-                        // Turn the relay off after delay to simulate button press
-                        delayPinWrite(config.LEFT_GARAGE_PIN, config.RELAY_OFF, callback);
-                        },
-                        function(err, results) {
-                        setTimeout(function() {
-                            // Close pin from further writing
-                            gpio.close(config.LEFT_GARAGE_PIN);
-                            // Return json
-                            res.json("ok");
-                            }, config.RELAY_TIMEOUT);
-                        }
+                console.log(sqs_message_body.Message);
+                /*async.series([
+                    function(callback) {
+                    // Open pin for output
+                    //gpio.open(config.PIN, "output", callback);
+                    },
+                    function(callback) {
+                    // Turn the relay on
+                    //gpio.write(config.PIN, config.RELAY_ON, callback);
+                    },
+                    function(callback) {
+                    // Turn the relay off after delay to simulate button press
+                    //delayPinWrite(config.PIN, config.RELAY_OFF, callback);
+                    },
+                    function(err, results) {
+                    setTimeout(function() {
+                        // Close pin from further writing
+                        //gpio.close(config.PIN);
+                        // Return json
+                        push.send("Door command successful", "Garage door has been opened/closed!");
+                        }, config.RELAY_TIMEOUT);
+                    }
                 ]); */
 
+                push.send("Door command successful", "Garage door has been opened/closed!");
                 //delete message from queue
                 sqs.deleteMessage({
                     "QueueUrl" : "https://sqs.us-east-1.amazonaws.com/990510411818/bells-walk-garage-door-queue",
@@ -65,4 +65,3 @@ function readMessage() {
         });
     }          
 readMessage();        
-
