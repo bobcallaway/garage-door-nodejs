f2d2e282c7beb98ac7b9aa2e700981b5ef90a94a
Bob Callaway <bob.callaway@gmail.com>
2013-10-18 14:30:30 -0400

    add php scripts

---
+++ operate.php
@@ -0,0 +1,33 @@
+<?php
+    include('amazonsns.class.php');
+
+    $result = "failed";
+    $message = "";
+
+    if ($_GET["key"]) {
+	try {
+            $topicArn = "arn:aws:sns:us-east-1:990510411818:bells-walk-garage-door";
+            
+            $AmazonSNS = new AmazonSNS("AKIAIPADUMAIHXBKKNKQ",htmlspecialchars($_GET["key"]));
+            
+            $AmazonSNS->publish($topicArn, '{ message: "operate" }');
+            
+            $result = "issued";
+            $message = "door command triggered";
+        } catch (Exception $e) {
+            $message = $e->getMessage();
+	}
+    }
+    else {
+        $message = "no key specified";
+    }
+?>
+<html>
+<head>
+<style>h1 {font-size:1000%;}</style>
+<title>Command <?php echo $result; ?>!</title>
+</head>
+<body>
+<h1><a href="javascript:window.close();"><?php echo $message; ?></a></h1>
+</body>
+</html>
